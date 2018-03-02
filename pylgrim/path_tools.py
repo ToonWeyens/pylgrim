c409ce8c45f7221c6372bffae4d59c7a5fa059f5
Daan van Vugt <daanvanvugt@gmail.com>
2018-03-02 16:57:44 +0100

    docs of path_tools

--- pylgrim/path_tools.py
+++ pylgrim/path_tools.py
@@ -1,15 +1,15 @@
 # Tools.
 #
-# TODO:
-#   add license.
-#
 # Author:
 #   Toon Weyens
 
-# prints a path with pretty formatting
-def print_path(path, max_path_len_for_print = 4):
-    # it depends on whether we trim or not
+def print_path(path, max_path_len_for_print = None):
+    """Pretty-print a path given as an iterable of strings.
+    Optionally trim if longer than max_path_len_for_print
+    """
     path_short = str(path[0])
+    if max_path_len_for_print is None:
+        max_path_len_for_print = len(path)
     if len(path) > max_path_len_for_print + 1:
         for p in range(1,max_path_len_for_print-1):
             path_short += ' ⇨ ' + str(path[p])
@@ -19,8 +19,8 @@ def print_path(path, max_path_len_for_print = 4):
             path_short += ' ⇨ ' + str(path[p])
     return path_short
 
-# counts elements in a path and returns them in a dictionary
 def count_elems(path):
+    """Count elements in a path and return a dictionary keyed with label"""
     res = dict()
     for n in path:
         res[n] = res.get(n,0)+1
